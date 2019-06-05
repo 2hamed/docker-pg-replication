17a45715a1b87c0e85157378fc0787b73b9bcca9
jahn <ioannis.kiwe@gmail.com>
2019-06-05 18:12:56 +0000

    Introduced PG_MASTER_HOST env var

--- slave/docker-entrypoint.sh
+++ slave/docker-entrypoint.sh
@@ -4,12 +4,12 @@ echo "*:*:*:$PG_REP_USER:$PG_REP_PASSWORD" > ~/.pgpass
 
 chmod 0600 ~/.pgpass
 
-until ping -c 1 -W 1 pg_master
+until ping -c 1 -W 1 ${PG_MASTER_HOST}
     do
         echo "Waiting for master to ping..."
         sleep 1s
 done
-until pg_basebackup -h pg_master -D ${PGDATA} -U ${PG_REP_USER} -vP -W
+until pg_basebackup -h ${PG_MASTER_HOST} -D ${PGDATA} -U ${PG_REP_USER} -vP -W
     do
         echo "Waiting for master to connect..."
         sleep 1s
@@ -21,7 +21,7 @@ set -e
 
 cat > ${PGDATA}/recovery.conf <<EOF
 standby_mode = on
-primary_conninfo = 'host=pg_master port=5432 user=$PG_REP_USER password=$PG_REP_PASSWORD'
+primary_conninfo = 'host=$PG_MASTER_HOST port=5432 user=$PG_REP_USER password=$PG_REP_PASSWORD'
 trigger_file = '/tmp/touch_me_to_promote_to_me_master'
 EOF
 chown postgres. ${PGDATA} -R
